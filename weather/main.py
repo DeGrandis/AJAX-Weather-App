32c609158b61aedb5198ba5993d552fd0d33cdae
Rob <robd@vt.edu>
2017-07-31 22:11:24 -0400

    Add files via upload

---
+++ weather/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='192.168.1.187', port=25565)
